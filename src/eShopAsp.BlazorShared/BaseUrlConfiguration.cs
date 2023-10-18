adcfa7abdcebfa0f9a66acd12020317ea8ed576b
Dang Hai <pecheule34713@gmail.com>
2023-10-19 00:28:08 +0700

    updates-BlazorShared

---
+++ src/eShopAsp.BlazorShared/BaseUrlConfiguration.cs
@@ -0,0 +1,8 @@
+namespace eShopAsp.BlazorShared;
+
+public class BaseUrlConfiguration 
+{
+    public const string CONFIG_NAME = "baseUrls";
+    public string ApiBase { get; set; }
+    public string WebBase { get; set; }
+}
