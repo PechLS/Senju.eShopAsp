164de877a1b479a70acba7ce7600eebb13d67add
Dang Hai <pecheule34713@gmail.com>
2023-10-19 11:49:17 +0700

    updates-entry-point

--- src/eShopAsp.PublicApi/Program.cs
+++ src/eShopAsp.PublicApi/Program.cs
@@ -1,6 +1,165 @@
+using System.Text;
+using eShopAsp.BlazorShared;
+using eShopAsp.Core.Constants;
+using eShopAsp.Core.Entities;
+using eShopAsp.Core.Interfaces;
+using eShopAsp.Core.Interfaces.Services;
+using eShopAsp.Core.Services;
+using eShopAsp.Infrastructure.Data;
+using eShopAsp.Infrastructure.Identity;
+using eShopAsp.Infrastructure.Logging;
+using eShopAsp.PublicApi;
+using eShopAsp.PublicApi.Middleware;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using MinimalApi.Endpoint.Configurations.Extensions;
+using MinimalApi.Endpoint.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpoints();
+builder.Logging.AddConsole();
+
+eShopAsp.Infrastructure.Dependencies.ConfigureServices(builder.Configuration, builder.Services);
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<AppIdentityDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
+builder.Services.AddScoped(typeof(IReadRepository<>), typeof(EfRepository<>));
+builder.Services.Configure<CatalogSettings>(builder.Configuration);
+
+var catalogSettings = builder.Configuration.Get<CatalogSettings>() ?? new CatalogSettings();
+builder.Services.AddSingleton<IUriComposer>(new UriComposer(catalogSettings));
+builder.Services.AddScoped(typeof(IAppLogger<>), typeof(LoggingAdapter<>));
+builder.Services.AddScoped<ITokenClaimsService, IdentityTokenClaimService>();
+
+var configSection = builder.Configuration.GetRequiredSection(BaseUrlConfiguration.CONFIG_NAME);
+builder.Services.Configure<BaseUrlConfiguration>(configSection);
+var baseUrlConfig = configSection.Get<BaseUrlConfiguration>();
+
+builder.Services.AddMemoryCache();
+
+var key = Encoding.ASCII.GetBytes(AuthorizationConstants.JWT_SECRET_KEY);
+builder.Services.AddAuthentication(config  => 
+{
+    config.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(config => 
+{
+    config.RequireHttpsMetadata = false;
+    config.SaveToken = true;
+    config.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        IssuerSigningKey = new SymmetricSecurityKey(key)
+    };
+});
+
+const string CORS_POLICY = "CorsPolicy";
+builder.Services.AddCors(options => 
+{
+    options.AddPolicy(name: CORS_POLICY,
+        corsPolicyBuilder => 
+        {
+            corsPolicyBuilder.WithOrigins(baseUrlConfig!.WebBase.Replace("host.docker.internal", "localhost").TrimEnd('/'));
+            corsPolicyBuilder.AllowAnyMethod();
+            corsPolicyBuilder.AllowAnyHeader();
+        });
+});
+
+builder.Services.AddControllers();
+builder.Services.AddAutoMapper(typeof(MappingProfile).Assembly);
+builder.Configuration.AddEnvironmentVariables();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c => 
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1"});
+    c.EnableAnnotations();
+    c.SchemaFilter<CustomSchemaFilters>();
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n
+                        Enter 'Bearer' [space] and then your token in the text input below.
+                        \r\n\r\nExample: 'Bearer 12345abcdef",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        },
+                        Scheme = "oauth2",
+                        Name = "Bearer",
+                        In = ParameterLocation.Header
+                    },
+                    new List<string>()
+                }
+            });
+});
+
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.Logger.LogInformation("PublicApi App created....");
+app.Logger.LogInformation("Seeding Database...");
+
+using (var scope = app.Services.CreateScope())
+{
+    var scopeProvider = scope.ServiceProvider;
+    try 
+    {
+        var catalogContext = scopeProvider.GetRequiredService<CatalogContext>();
+        await CatalogContextSeed.SeedAsync(catalogContext, app.Logger);
+        var userManager = scopeProvider.GetRequiredService<UserManager<ApplicationUser>>();
+        var roleManager = scopeProvider.GetRequiredService<RoleManager<IdentityRole>>();
+        var identityContext = scopeProvider.GetRequiredService<AppIdentityDbContext>();
+        await AppIdentityDbContextSeed.SeedAsync(identityContext, userManager, roleManager);
+    }
+    catch (Exception ex)
+    {
+        app.Logger.LogError(ex, "An error occurred seeding the DB");
+    }
+}
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseMiddleware<ExceptionMiddleware>();
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseCors(CORS_POLICY);
+app.UseAuthorization();
+app.UseSwagger();
+app.UseSwaggerUI(c => 
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "MY API v1");
+});
+
+app.MapControllers();
+app.MapEndpoints();
+app.Logger.LogInformation("LAUNCHING PublicApi");
+
 
 app.Run();
+
+
+public partial class Program {}
